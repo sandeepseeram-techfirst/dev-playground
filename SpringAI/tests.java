709d311adf6c64c5cdbafebbb53ae2706b1cca2d
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2025-11-24 17:22:31 +0530

    Spring AI Workshop

--- SpringAI/tests.java
+++ SpringAI/tests.java
@@ -1,6 +1,6 @@
 package ch02;
 
-import ch02.service.FirstChatService;
+import ch02.service.FirstChatService; 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
