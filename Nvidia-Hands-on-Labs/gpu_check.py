6c86f3ec9a232bc69c5c4011a331d0771b009434
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2026-04-13 22:57:53 +0530

    nvidia-hands-on-labs

--- Nvidia-Hands-on-Labs/gpu_check.py
+++ Nvidia-Hands-on-Labs/gpu_check.py
@@ -7,4 +7,4 @@ gpu_name = torch.cuda.get_device_name(0)
 print(f"GPU: {gpu_name}")
 
 total_vram_gb = torch.cuda.get_device_properties(0).total_mem / 1e9
-print(f"VRAM: {total_vram_gb:.1f} GB")
+print(f"VRAM: {total_vram_gb:.1f} GB") 
